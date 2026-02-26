94f79d6910d28689a324474cf7da4fc126039150
FrederikvSvane <fsvane@hotmail.com>
2026-02-26 15:37:09 +0100

    flow based VAE prior

--- src/vae_bernoulli.py
+++ src/vae_bernoulli.py
@@ -17,7 +17,7 @@ class GaussianPrior(nn.Module):
         """
         Define a Gaussian prior distribution with zero mean and unit variance.
 
-                Parameters:
+        Parameters:
         M: [int] 
            Dimension of the latent space.
         """
@@ -34,6 +34,217 @@ class GaussianPrior(nn.Module):
         prior: [torch.distributions.Distribution]
         """
         return td.Independent(td.Normal(loc=self.mean, scale=self.std), 1)
+
+class GaussianBase(nn.Module):
+    def __init__(self, D):
+        """
+        Define a Gaussian base distribution with zero mean and unit variance.
+
+                Parameters:
+        M: [int] 
+           Dimension of the base distribution.
+        """
+        super(GaussianBase, self).__init__()
+        self.D = D
+        self.mean = nn.Parameter(torch.zeros(self.D), requires_grad=False)
+        self.std = nn.Parameter(torch.ones(self.D), requires_grad=False)
+
+    def forward(self):
+        """
+        Return the base distribution.
+
+        Returns:
+        prior: [torch.distributions.Distribution]
+        """
+        return td.Independent(td.Normal(loc=self.mean, scale=self.std), 1)
+
+class MaskedCouplingLayer(nn.Module):
+    """
+    An affine coupling layer for a normalizing flow.
+    """
+
+    def __init__(self, scale_net, translation_net, mask):
+        """
+        Define a coupling layer.
+
+        Parameters:
+        scale_net: [torch.nn.Module]
+            The scaling network that takes as input a tensor of dimension `(batch_size, feature_dim)` and outputs a tensor of dimension `(batch_size, feature_dim)`.
+        translation_net: [torch.nn.Module]
+            The translation network that takes as input a tensor of dimension `(batch_size, feature_dim)` and outputs a tensor of dimension `(batch_size, feature_dim)`.
+        mask: [torch.Tensor]
+            A binary mask of dimension `(feature_dim,)` that determines which features (where the mask is zero) are transformed by the scaling and translation networks.
+        """
+        super(MaskedCouplingLayer, self).__init__()
+        self.scale_net = scale_net
+        self.translation_net = translation_net
+        self.mask = nn.Parameter(mask, requires_grad=False)
+
+    def forward(self, z):
+        """
+        Transform a batch of data through the coupling layer (from the base to data).
+
+        Parameters:
+        z: [torch.Tensor]
+            The input to the transformation of dimension `(batch_size, feature_dim)`
+        Returns:
+        x: [torch.Tensor]
+            The output of the transformation of dimension `(batch_size, feature_dim)`
+        sum_log_det_J: [torch.Tensor]
+            The sum of the log determinants of the Jacobian matrices of the forward transformations of dimension `(batch_size, feature_dim)`.
+        """
+        x = self.mask * z + (1 - self.mask) * (z * torch.exp(self.scale_net(self.mask * z) + self.translation_net(self.mask * z)))
+        log_det_J = torch.sum((1-self.mask) * self.scale_net(self.mask * z), dim=1)
+        return x, log_det_J
+    
+    def inverse(self, x):
+        """
+        Transform a batch of data through the coupling layer (from data to the base).
+
+        Parameters:
+        x: [torch.Tensor]
+            The input to the inverse transformation of dimension `(batch_size, feature_dim)`
+        Returns:
+        z: [torch.Tensor]
+            The output of the inverse transformation of dimension `(batch_size, feature_dim)`
+        sum_log_det_J: [torch.Tensor]
+            The sum of the log determinants of the Jacobian matrices of the inverse transformations.
+        """
+        z = self.mask * x + (1 - self.mask) * ((x - self.translation_net(self.mask * x)) * torch.exp(-self.scale_net(self.mask * x)))
+        log_det_J = -torch.sum((1 - self.mask)*self.scale_net(self.mask * x), dim=1)
+        return z, log_det_J
+
+class Flow(nn.Module):
+    def __init__(self, base, transformations):
+        """
+        Define a normalizing flow model.
+        
+        Parameters:
+        base: [torch.distributions.Distribution]
+            The base distribution.
+        transformations: [list of torch.nn.Module]
+            A list of transformations to apply to the base distribution.
+        """
+        super(Flow, self).__init__()
+        self.base = base
+        self.transformations = nn.ModuleList(transformations)
+
+    def forward(self, z):
+        """
+        Transform a batch of data through the flow (from the base to data).
+        
+        Parameters:
+        z: [torch.Tensor]
+            The input to the transformation of dimension `(batch_size, feature_dim)`
+        Returns:
+        x: [torch.Tensor]
+            The output of the transformation of dimension `(batch_size, feature_dim)`
+        sum_log_det_J: [torch.Tensor]
+            The sum of the log determinants of the Jacobian matrices of the forward transformations.            
+        """
+        sum_log_det_J = 0
+        for T in self.transformations:
+            x, log_det_J = T(z)
+            sum_log_det_J += log_det_J
+            z = x
+        return x, sum_log_det_J
+    
+    def inverse(self, x):
+        """
+        Transform a batch of data through the flow (from data to the base).
+
+        Parameters:
+        x: [torch.Tensor]
+            The input to the inverse transformation of dimension `(batch_size, feature_dim)`
+        Returns:
+        z: [torch.Tensor]
+            The output of the inverse transformation of dimension `(batch_size, feature_dim)`
+        sum_log_det_J: [torch.Tensor]
+            The sum of the log determinants of the Jacobian matrices of the inverse transformations.
+        """
+        sum_log_det_J = 0
+        for T in reversed(self.transformations):
+            z, log_det_J = T.inverse(x)
+            sum_log_det_J += log_det_J
+            x = z
+        return z, sum_log_det_J
+    
+    def log_prob(self, x):
+        """
+        Compute the log probability of a batch of data under the flow.
+
+        Parameters:
+        x: [torch.Tensor]
+            The data of dimension `(batch_size, feature_dim)`
+        Returns:
+        log_prob: [torch.Tensor]
+            The log probability of the data under the flow.
+        """
+        z, log_det_J = self.inverse(x)
+        return self.base().log_prob(z) + log_det_J
+    
+    def sample(self, sample_shape=(1,)):
+        """
+        Sample from the flow.
+
+        Parameters:
+        n_samples: [int]
+            Number of samples to generate.
+        Returns:
+        z: [torch.Tensor]
+            The samples of dimension `(n_samples, feature_dim)`
+        """
+        z = self.base().sample(sample_shape)
+        return self.forward(z)[0]
+    
+    def loss(self, x):
+        """
+        Compute the negative mean log likelihood for the given data bath.
+
+        Parameters:
+        x: [torch.Tensor] 
+            A tensor of dimension `(batch_size, feature_dim)`
+        Returns:
+        loss: [torch.Tensor]
+            The negative mean log likelihood for the given data batch.
+        """
+        return -torch.mean(self.log_prob(x))
+    
+class FlowPrior(nn.Module):
+    def __init__(self, M, num_flows):
+        """
+        Define a flow based prior distribution
+        
+        Parameters:
+        M: [int]
+            Dimension of the latent space.
+        
+        num_flows: [int]
+            Amount of transformations to apply to the base distribution
+        """
+        super().__init__()
+        
+        self.base = GaussianBase(M)
+        # Two complementary masks                                                             
+        mask1 = torch.zeros(M)
+        mask1[::2] = 1          # [1, 0, 1, 0, ...]
+
+        mask2 = torch.ones(M)
+        mask2[::2] = 0           # [0, 1, 0, 1, ...]
+        
+        hidden_dim_size = 128
+        transformations = []
+        for i in range(num_flows):
+            mask = mask1 if i % 2 == 0 else mask2
+            s_net = nn.Sequential(nn.Linear(M, hidden_dim_size), nn.ReLU(), nn.Linear(hidden_dim_size, M))
+            t_net = nn.Sequential(nn.Linear(M, hidden_dim_size), nn.ReLU(), nn.Linear(hidden_dim_size, M))
+            transformations.append(MaskedCouplingLayer(s_net, t_net, mask))
+        
+        self.flow = Flow(self.base, transformations)
+        
+    # Flow layers
+    def forward(self):
+        return self.flow
 
 
 class GaussianEncoder(nn.Module):
@@ -121,7 +332,7 @@ class VAE(nn.Module):
         """
         q = self.encoder(x)
         z = q.rsample()
-        elbo = torch.mean(self.decoder(z).log_prob(x) - td.kl_divergence(q, self.prior()), dim=0)
+        elbo = torch.mean(self.decoder(z).log_prob(x) + self.prior().log_prob(z) - q.log_prob(z), dim=0)
         return elbo
 
     def sample(self, n_samples=1):
@@ -215,7 +426,7 @@ if __name__ == "__main__":
 
     # Define prior distribution
     M = args.latent_dim
-    prior = GaussianPrior(M)
+    prior = FlowPrior(M, num_flows=8)
 
     # Define encoder and decoder networks
     encoder_net = nn.Sequential(
